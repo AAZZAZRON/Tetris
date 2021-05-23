2360373f54a17356c830e487d3e273b45f468a4d
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-05-23 19:16:50 -0400

    positioning

--- mainMenu.js
+++ mainMenu.js
@@ -1,6 +1,7 @@
 var body = document.getElementById("body");
 window.onload = function() {
     mainMenu();
+    configureGameBoard();
 };
 
 function mainMenu() {
@@ -10,4 +11,17 @@ function mainMenu() {
     document.getElementById("MainMenu").hidden = false;
     document.getElementById("GamePlay").hidden = true;
     document.getElementById("Configurations").hidden = true;
+}
+
+function configureGameBoard() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    document.getElementById("holdSpot").style.top = h / 2 - 503;
+    document.getElementById("holdSpot").style.left = w / 2 - 466;
+    document.getElementById("holdtext").style.top = h / 2 - 503;
+    document.getElementById("holdtext").style.left = w / 2 - 466;
+    document.getElementById("queue").style.top = h / 2 - 503;
+    document.getElementById("queue").style.left = w / 2 + 263;
+    document.getElementById("queuetext").style.top = h / 2 - 503;
+    document.getElementById("queuetext").style.left = w / 2 + 263;
 }
